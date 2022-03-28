defba9b383ee6b1d44b9f897cc4c8bc4dca506cc
Luigi311 <luigi311.lg@gmail.com>
2022-03-27 21:32:00 -0600

    Revert build-plugins.ps1

--- build-plugins.ps1
+++ build-plugins.ps1
@@ -1,13 +1,5 @@
 $pluginSrcDir = '../FileFlowsPlugins'
 
-# Create Server Plugins Folder
-$FolderName = "deploy/Plugins"
-if (!(Test-Path -Path $FolderName)) {
-    #PowerShell Create directory if not exists
-    New-Item $FolderName -ItemType Directory
-    Write-Host "Folder Created successfully"
-}
-
 # build plugin
 # build 0.0.1.0 so included one is always greater
 dotnet build Plugin\Plugin.csproj /p:WarningLevel=1 --configuration Release  /p:AssemblyVersion=0.0.1.0 /p:Version=0.0.1.0 /p:CopyRight=$copyright --output $pluginSrcDir
@@ -16,5 +8,5 @@ Remove-Item ../../FileFlowsPlugins/FileFlows.Plugin.deps.json -ErrorAction Silen
 dotnet publish PluginInfoGenerator\PluginInfoGenerator.csproj --configuration Release --output $pluginSrcDir/build/utils/PluginInfoGenerator
 
 Push-Location ../FileFlowsPlugins/build
-./buildplugins.ps1 "../../FileFlows/$FolderName"
+./buildplugins.ps1 '../../FileFlows/Server/Plugins'
 Pop-Location
