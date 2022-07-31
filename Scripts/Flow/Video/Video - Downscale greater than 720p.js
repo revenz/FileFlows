59e1b0f01ae7cd19a9a17c70efe19e8042aae41f
John Andrews <john@reven.nz>
2022-07-31 12:54:20 +1200

    updated downscale to work with ffmpeg builder and added 720p downscale script

--- Scripts/Flow/Video/Video - Downscale greater than 720p.js
+++ Scripts/Flow/Video/Video - Downscale greater than 720p.js
@@ -1,11 +1,11 @@
 /**
- * If a video's resolution is greater than 1080p, this script will update the FFMPEG Builder video track with a filter 
- * to downscale the video to 1080p when the FFMPEG Builder Executor runs.
+ * If a video's resolution is greater than 720p, this script will update the FFMPEG Builder video track with a filter 
+ * to downscale the video to 720p when the FFMPEG Builder Executor runs.
  * @author John Andrews
- * @revision 3
+ * @revision 1
  * @minimumVersion 1.0.0.0
- * @output Video is greater than 1080p, FFMPEG Builder Updated
- * @output Video is not greater than 1080p
+ * @output Video is greater than 720p, FFMPEG Builder Updated
+ * @output Video is not greater than 720p
  */
 function Script()
 {
@@ -24,13 +24,13 @@ function Script()
   }
 
 
-  if (video.Stream.Width < 1921)
+  if (video.Stream.Width < 1281)
   {
     Logger.ILog('Do not need to downscale');
     return 2;
   }
   
   Logger.ILog(`Need to downscale from ${video.Width}x${video.Height}`);
-  video.Filter.Add(`scale=1920:-2:flags=lanczos`);
+  video.Filter.Add(`scale=1280:-2:flags=lanczos`);
   return 1;
 }
