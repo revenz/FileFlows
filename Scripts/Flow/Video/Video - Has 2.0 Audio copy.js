07ea29e7afb7a5afcaddc7602bd00ec4ba941182
john <john@reven.nz>
2023-01-29 15:33:39 +1300

    scripts

---
+++ Scripts/Flow/Video/Video - Has 2.0 Audio copy.js
@@ -0,0 +1,25 @@
+/**
+ * Determines if a video has a 2.0 (stereo) channel track
+ * @author John Andrews
+ * @revision 1
+ * @minimumVersion 1.0.0.0
+ * @output Does have 2.0
+ * @output Does not have 2.0
+ */
+function Script() 
+{
+  let audioStreams = Variables.vi?.VideoInfo?.AudioStreams;
+  if (!audioStreams) {
+    Logger.ILog("no audio streams");
+    return 2;
+  }
+
+  for (let i = 0; i < audioStreams.length; i++) {
+    let audio = audioStreams[i];
+    Logger.ILog("Audio channel found: " + audio.Channels);
+    if (audio.Channels === 2)
+      return 1;
+  }
+
+  return 2;
+}
