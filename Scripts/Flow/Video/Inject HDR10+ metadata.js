b3d0ecd1dc7801e651f9c1dce7b1be6736d698c5
GrimJu <55273793+GrimJu@users.noreply.github.com>
2023-07-30 01:46:16 +0200

    Create Inject HDR10+ metadata.js

---
+++ Scripts/Flow/Video/Inject HDR10+ metadata.js
@@ -0,0 +1,103 @@
+/**
+ * Injects The Hdr10+ Metadata from the original file into the current working file
+ * @author https://github.com/GrimJu
+ * @version 0.0.2
+ * @param {string} hdr10plus_tool_path Mapped path to the hdr10plus_tool executable (https://github.com/quietvoid/hdr10plus_tool)
+ * @output Success, metadata was injected and new working file was created 
+ * @output Fail
+ */
+function Script(hdr10plus_tool_path)
+{
+    if( !hdr10plus_tool_path){
+        Logger.ELog("No hdr10plus_tool found");
+        return 2;
+    }
+
+    let ffmpeg = Flow.GetToolPath('ffmpeg');
+    let process = Flow.Execute({
+        command: ffmpeg,
+        argumentList: [
+            '-i',
+            Variables.file.Orig.FullName,
+            '-map',
+            '0:v:0',
+            '-c',
+            'copy',
+            "-nostdin",
+            "-y",
+            "originalHEVCStream.hevc"
+        ]
+    });
+
+    processLogs(process);
+
+
+    process = Flow.Execute({
+        command: hdr10plus_tool_path,
+        argumentList:[
+            "extract", "-i","originalHEVCStream.hevc","-o","metadata.json"
+        ]
+    })
+    processLogs(process);
+
+    process = Flow.Execute({
+        command: ffmpeg,
+        argumentList: [
+            '-i',
+            Variables.file.FullName,
+            '-map',
+            '0:v:0',
+            '-c',
+            'copy',
+            "-nostdin",
+            "-y",
+            "newHEVCStream.hevc"
+        ]
+    });
+    processLogs(process);
+
+    process = Flow.Execute({
+        command: hdr10plus_tool_path,
+        argumentList:[
+            "inject","-j","metadata.json", "-i","newHEVCStream.hevc","-o","outputHEVCStream.hevc"
+        ]
+    })
+    processLogs(process);
+
+    process = Flow.Execute({
+        command: ffmpeg,
+        argumentList: [
+            "-i",
+            "outputHEVCStream.hevc",
+            '-i',
+            Variables.file.FullName,
+            '-map',
+            '0:v:0',
+            "-map",
+            "1",
+            "-map", 
+            "-1:v:0",
+            '-c',
+            'copy',
+            "-nostdin",
+            "-y",
+            `output.${Variables.file.Extension}`
+        ]
+    });
+    processLogs(process);
+
+    Flow.SetWorkingFile(`output.${Variables.file.Extension}`)
+    return 1;
+} 
+
+function processLogs(process){
+    if(process.standardOutput)
+        Logger.ILog('Standard output: ' + process.standardOutput);
+    if(process.standardError)
+        Logger.ILog('Standard error: ' + process.standardError);
+
+    if(process.exitCode !== 0){
+        Logger.ELog('Failed processing: ' + process.exitCode);
+        return 2;
+    }
+}
