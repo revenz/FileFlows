46f0284fc540586001f08eba6c8396057f5ba00e
Schmidsfeld <68500293+Schmidsfeld@users.noreply.github.com>
2023-07-29 14:32:43 +0200

    Update Video - FPS greater than.js
    Added some workarround for way to high fps

--- Scripts/Flow/Video/Video - FPS greater than.js
+++ Scripts/Flow/Video/Video - FPS greater than.js
@@ -1,26 +1,35 @@
 /**
  * Checks if a video's fps is greater than specified.
- * @author Alexander
- * @revision 2
+ * @author Alexander von Schmidsfeld
+ * @revision 3
  * @minimumVersion 1.0.0.0
  * @param {int} MaxFps The maximum to check in fps
  * @output Video's fps is greater than specified
  * @output Video's fps is not greater than specified
  */
-function Script(MaxFps)
-{
-  // check if the fps for a video is over a certain amount
-  let MAX_FPS = MaxFps * 1.0; 
 
-  let vi = Variables.vi?.VideoInfo;
-  if(!vi || !vi.VideoStreams || !vi.VideoStreams[0])
-    return -1; // no video information found
-
-  // get the video stream
-  let fps = vi.VideoStreams[0].FramesPerSecond;
-
-  // check if the fps is over the maximum bitrate
-  if(fps > MAX_FPS)
-    return 1; // it is, so call output 1
-  return 2; // it isn't so call output 2
-}
+ function Script(MaxFps)
+ {
+   // check if the fps for a video is over a certain amount
+   let MAX_FPS = MaxFps * 1.0; 
+ 
+   let vi = Variables.vi?.VideoInfo;
+   if(!vi || !vi.VideoStreams || !vi.VideoStreams[0])
+     return -1; // no video information found
+ 
+   // get the video stream
+   let fps = Math.ceil(vi.VideoStreams[0].FramesPerSecond);
+   // Bugfix for way to high framerate detected by loosing the decimal point
+   if(fps > 200) {
+    fps = fps/100
+   }
+ 
+   // check if the fps is over the maximum bitrate
+   if(fps > MAX_FPS) {
+     // Logger.DLog(fps);
+     Logger.ILog('The video framerate is ${fps} above the threshold of ${MAX_FPS}');
+     return 1; // it is, so call output 1
+     }
+   Logger.ILog('The video framerate is ${fps} below the threshold of ${MAX_FPS}');
+   return 2; // it isn't so call output 2 also for unknown fps
+ }
