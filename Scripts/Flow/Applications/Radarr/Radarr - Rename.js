1effd8cfdaef48815f83a1d4fd1ad8c8f69c581c
Feramance <techagius2093@gmail.com>
2024-05-30 10:13:39 +0200

    New scripts to setect and rename Series/Movies through Sonarr/Radarr

---
+++ Scripts/Flow/Applications/Radarr/Radarr - Rename.js
@@ -0,0 +1,23 @@
+import { Radarr } from 'Shared/Radarr';
+/**
+ * This script will search the active queue and blocklist and research
+ * For use alongside this strategy https://fileflows.com/docs/guides/sonarr-radarr
+ * @author Shaun Agius
+ * @version 1.0.0
+ * @revision 1
+ * @param {string} URI Radarr root URI and port (e.g. http://radarr:1234)
+ * @param {string} ApiKey API Key
+ * @output Item renamed
+ * @output Item not found
+ */
+function Script(URI, ApiKey) {
+    let radarr = new Radarr(URI, ApiKey);
+    let movie = radarr.getMovieByPath(Variables.folder.Name);
+    if (!movie)
+        return 2;
+    Logger.ILog(`Renaming ${movie.title}`);
+    let endpoint = `rename?movieId=${movie.id}`;
+    let response = radarr.fetchJson(endpoint);
+    Logger.ILog(`Response ${response}`);
+    return 1;
+}
