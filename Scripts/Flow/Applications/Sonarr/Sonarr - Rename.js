933fe5385664375bc2534c4933665e33970b9d86
ant385525 <ant385525@yahoo.com>
2024-06-08 20:54:33 -0700

    Something went horribly wrong there, I'm terribly sorry I don't use Github often.

--- Scripts/Flow/Applications/Sonarr/Sonarr - Rename.js
+++ Scripts/Flow/Applications/Sonarr/Sonarr - Rename.js
@@ -11,18 +11,80 @@ import { Sonarr } from 'Shared/Sonarr';
  * @output Item renamed
  * @output Item not found
  */
+
 function Script(URI, ApiKey) {
     let sonarr = new Sonarr(URI, ApiKey);
-    let folder = Variables.folder.FullName
-    var season
-    if (folder.includes("Season"))
-        season = folder.indexOf('Season')-1
-    else
-    {
-        if (folder.includes("/"))
-            season = folder.lastIndexOf("/")
-        else if (folder.includes("\\"))
-            season = folder.lastIndexOf("\\")
+    const folderPath = Variables.folder.FullName;
+    const currentFileName = Variables.file.Name;
+    let newFileName = null;
+    let episodeFileId = null;
+
+    // Find series name from sonarr
+    let series = findSeries(folderPath, sonarr);
+
+    if (!series) {
+        Logger.WLog('Series not found for path: ' + folderPath);
+        return 2;
+    }
+
+    try {
+        // Ensure series is refreshed before renaming
+        let refreshBody = {
+            seriesId: series.id
+        }
+        let refreshData = sonarr.sendCommand('RescanSeries', refreshBody)
+        Logger.ILog(`Series refreshed`);
+
+        // Wait for the completion of the refresh scan
+        let refreshCompleted = sonarr.waitForCompletion(refreshData.id, sonarr);
+        if (!refreshCompleted) {
+            Logger.ILog('Refresh not completed');
+            return -1;
+        }
+
+        let renamedEpisodes = fetchRenamedFiles(series.id, sonarr);
+        if (!renamedEpisodes) {
+            Logger.ILog('No episodes need to be renamed');
+            return 2;
+        }
+
+        Logger.ILog(`Searching for an episode previously named ${currentFileName}`);
+        renamedEpisodes.every((episode) => {
+            if (episode.existingPath.endsWith(currentFileName)) {
+                episodeFileId = episode.episodeFileId;
+                newFileName = System.IO.Path.GetFileName(episode.newPath);
+                Logger.ILog(`Found it, renaming file ${episodeFileId} to ${newFileName}`);
+                return false;
+            }
+            return true;
+        });
+
+        if (newFileName === null) {
+            Logger.WLog('No matching episode found to rename.');
+            return 2;
+        }
+
+        let renameBody = {
+            seriesId: series.id,
+            files: [episodeFileId]
+        }
+        let renameResponse = sonarr.sendCommand('RenameFiles', renameBody, URI, ApiKey);
+        let renameCompleted = sonarr.waitForCompletion(renameResponse.id);
+
+        if (!renameCompleted) {
+            Logger.ILog('Rename not completed');
+            return -1;
+        }
+        Logger.ILog(`Episode ${episodeFileId} successfully renamed. Setting as working file.`)
+
+        // Sonarr has successfully renamed the file, set new filename as working directory
+        let newFilePath = System.IO.Path.Combine(Variables.folder.FullName, newFileName);
+        Flow.SetWorkingFile(newFilePath);
+        return 1;
+
+    } catch (error) {
+        Logger.WLog('Error: ' + error.message);
+        return -1;
     }
 }
 
