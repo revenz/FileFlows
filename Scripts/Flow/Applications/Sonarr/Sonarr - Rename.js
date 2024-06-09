45b9c9e38f4643beb50a6f58a8a4863fafadf388
ant385525 <ant385525@yahoo.com>
2024-06-08 20:19:20 -0700

    Add former author and update revision number

--- Scripts/Flow/Applications/Sonarr/Sonarr - Rename.js
+++ Scripts/Flow/Applications/Sonarr/Sonarr - Rename.js
@@ -2,8 +2,9 @@ import { Sonarr } from 'Shared/Sonarr';
 /**
  * This script will send a rename command to Sonarr
  * @author Anthony Clerici
+ * @author Shaun Agius
  * @version 1.0.0
- * @revision 1
+ * @revision 5
  * @param {string} URI Sonarr root URI and port (e.g. http://sonarr:1234)
  * @param {string} ApiKey API Key
  * @output Item renamed
