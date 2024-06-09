8c170f60133d710c6f1eda82241e5feddad86a33
ant385525 <121154142+ant385525@users.noreply.github.com>
2024-06-08 22:38:39 -0700

    Fix typo in description

--- Scripts/Flow/Applications/Radarr/Radarr - Rename.js
+++ Scripts/Flow/Applications/Radarr/Radarr - Rename.js
@@ -3,7 +3,7 @@ import { Radarr } from 'Shared/Radarr';
  * @author Anthony Clerici
  * @author Shaun Agius
  * @uid bd6f02c8-e650-4916-bcae-46f382d20388
- * @descripttion This script will send a rename command to Radarr
+ * @description This script will send a rename command to Radarr
  * @revision 6
  * @param {string} URI Radarr root URI and port (e.g. http://radarr:1234)
  * @param {string} ApiKey API Key
