52999651ee15b1846942ca78e1f2596b07fe5bcb
Feramance <38938175+Feramance@users.noreply.github.com>
2024-07-23 16:36:57 +0200

    Update Video - Delete Non-Original Language Audio.js revision number

--- Scripts/Flow/Video/Video - Delete Non-Original Language Audio.js
+++ Scripts/Flow/Video/Video - Delete Non-Original Language Audio.js
@@ -2,7 +2,7 @@
  * @author John Andrews
  * @author Shaun Agius
  * @uid d3d80753-4c85-4202-af33-ba73e585c771
- * @revision 7
+ * @revision 8
  * @description Checks the "Movie Lookup"/"TV Show Lookup" information for original language, and will delete any audio tracks with languages set that do not match the original language.  Requires the "Movie Lookup"/"TV Show Lookup" node to be executed first to work
  * @param {bool} TreatUnknownAsBad Treat a track with no language set as a bad language and do not include it, otherwise it will be treated as a good track
  * @param {bool} KeepFirstAudio If no matching langauges are found, keep the first audio track, otherwise all audio could be removed
