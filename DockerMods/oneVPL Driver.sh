3f525d8ff333a97ddb2d3ffc89aa35cc315cf62c
Lawrence Curtis <lawrencecurtis@yellowpad.co.uk>
2024-09-04 22:42:18 +0100

    Remove the word intel

--- DockerMods/oneVPL Driver.sh
+++ DockerMods/oneVPL Driver.sh
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------------------------------
-# Name: Intel-VPL-Driver
+# Name: oneVPL Driver
 # Description: This is only required if you have the onevpl Intel driver installed, most systems do not.
 # Author: John Andrews / Lawrence
 # Revision: 3
