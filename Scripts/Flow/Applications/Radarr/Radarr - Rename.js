de560e2ffd03eaa1bc77ff9faed06ff82fbaee3e
ant385525 <ant385525@yahoo.com>
2024-12-22 01:20:18 -0800

    Fix broken new file path finder

--- Scripts/Flow/Applications/Radarr/Radarr - Rename.js
+++ Scripts/Flow/Applications/Radarr/Radarr - Rename.js
@@ -3,7 +3,7 @@ import { Radarr } from 'Shared/Radarr';
 /**
  * @description This script will send a rename command to Radarr
  * @author Shaun Agius, Anthony Clerici
- * @revision 10
+ * @revision 11
  * @param {string} URI Radarr root URI and port (e.g. http://radarr:7878)
  * @param {string} ApiKey API Key
  * @output Item renamed successfully
@@ -11,13 +11,15 @@ import { Radarr } from 'Shared/Radarr';
  * @output Item not found
  */
 function Script(URI, ApiKey) {
+    // Remove trailing / from URI
+    URI = URI.replace(/\/$/, '');
     let radarr = new Radarr(URI, ApiKey);
     let folderPath = Variables.folder.FullName;
     let currentFileName = Variables.file.Name;
     let newFileName = null;
 
     // Find movie name from radarr
-    let movie = findMovie(folderPath, radarr);
+    let [movie, basePath] = findMovie(folderPath, radarr);
 
     if (!movie) {
         Logger.WLog('Movie not found for path: ' + folderPath);
@@ -75,7 +77,12 @@ function Script(URI, ApiKey) {
         }
         
         newFileName = renamedMovie.newPath;
-        Logger.ILog(`Found it, renaming file to ${newFileName}`);
+        // Get differences in the path
+        Logger.ILog(`Base path: ${basePath}`);
+        // Construct new filepath
+        Logger.ILog(`New path: ${newFileName}`);
+        newFilePath = System.IO.Path.Combine(basePath, newFileName);
+        Logger.ILog(`Found it, renaming file ${newEpisodeFileId} to ${newFilePath}`);
 
         if (newFileName === null) {
             Logger.WLog('No matching movie found to rename.');
@@ -98,7 +105,6 @@ function Script(URI, ApiKey) {
         Logger.ILog(`Movie ${movie.id} successfully renamed. Setting as working file.`)
 
         // Radarr has successfully renamed the file, set new filename as working directory
-        let newFilePath = System.IO.Path.Combine(Variables.folder.FullName, newFileName);
         Flow.SetWorkingFile(newFilePath);
         return 1;
 
@@ -129,7 +135,7 @@ function findMovie(filePath, radarr) {
         if (movieFolders[currentFolder]) {
             movie = movieFolders[currentFolder];
             Logger.ILog('Movie found: ' + movie.id);
-            return movie;
+            return [movie, currentPath];
         }
 
         // Log the path where the movie was not found and move up one directory
@@ -137,9 +143,9 @@ function findMovie(filePath, radarr) {
         currentPath = System.IO.Path.GetDirectoryName(currentPath);
         if (!currentPath) {
             Logger.WLog('Unable to find movie file at path ' + filePath);
-            return null;
+            return [null, null];
         }
     }
 
-    return null;
+    return [null, null];
 }
