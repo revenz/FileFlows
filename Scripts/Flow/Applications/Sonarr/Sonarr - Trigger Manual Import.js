adc2f156b958f7624ef45e30973c32e1687e7118
Idan Bush <ibushari@outlook.com>
2024-12-22 14:38:27 +0000

    bump version

--- Scripts/Flow/Applications/Sonarr/Sonarr - Trigger Manual Import.js
+++ Scripts/Flow/Applications/Sonarr/Sonarr - Trigger Manual Import.js
@@ -5,7 +5,7 @@ import { Sonarr } from 'Shared/Sonarr';
  * @uid 544e1d8f-c3b5-4d1a-8f2c-5fdf24126100
  * @description Trigger Sonarr to manually import the media, run after last file in folder moved.
  * @author iBuSH
- * @revision 1
+ * @revision 2
  * @param {string} URL Sonarr root URL and port (e.g. http://sonarr:8989)
  * @param {string} ApiKey Sonarr API Key
  * @param {string} ImportPath The output path for import triggering (default Working File)
