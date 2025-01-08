70ebbc097a8070ed658bed184fef9d60a7669c8a
Vincent Courcelle <github.me@5en1.com>
2025-01-08 09:28:50 +0400

    Fix Radarr - Get Original Language.js -- missing URL and API key

--- Scripts/Flow/Applications/Radarr/Radarr - Get Original Language.js
+++ Scripts/Flow/Applications/Radarr/Radarr - Get Original Language.js
@@ -7,14 +7,16 @@ import { Radarr } from '../../../Shared/Radarr';
  * @revision 6
  * @param {string} Path The full file path to lookup in Radarr
  * @param {bool} ISO2 If ISO-639-2 should be returned, otherwise ISO-639-1 will be used
+ * @param {string} URL Radarr root URL and port (e.g., http://radarr:1234)
+ * @param {string} ApiKey API Key for Radarr
  * @output The language was found and stored in the variable OriginalLanguage
  * @output The language was not found
  */
-function Script(Path, ISO2)
+function Script(Path, ISO2, URI, ApiKey)
 {
     if(!Path)
         return 2;
-    const radarr = new Radarr();
+    const radarr = new Radarr(URI, ApiKey);
     try
     {
         let language = radarr.getOriginalLanguageFromPath(Path.toString());
