22c7f81b062879f9ebef68453f86d43db77e2dbe
Vincent Courcelle <github.me@5en1.com>
2025-01-08 09:36:33 +0400

    Invert args order to align with other scripts

--- Scripts/Flow/Applications/Radarr/Radarr - Get Original Language.js
+++ Scripts/Flow/Applications/Radarr/Radarr - Get Original Language.js
@@ -5,14 +5,14 @@ import { Radarr } from '../../../Shared/Radarr';
  * @uid 3915f110-4b07-4e11-b7b9-50de3f5a1255
  * @description Lookups a file in Radarr and gets its original language ISO-693-1 code for it
  * @revision 6
- * @param {string} Path The full file path to lookup in Radarr
- * @param {bool} ISO2 If ISO-639-2 should be returned, otherwise ISO-639-1 will be used
  * @param {string} URL Radarr root URL and port (e.g., http://radarr:1234)
  * @param {string} ApiKey API Key for Radarr
+ * @param {string} Path The full file path to lookup in Radarr
+ * @param {bool} ISO2 If ISO-639-2 should be returned, otherwise ISO-639-1 will be used
  * @output The language was found and stored in the variable OriginalLanguage
  * @output The language was not found
  */
-function Script(Path, ISO2, URI, ApiKey)
+function Script(URI, ApiKey, Path, ISO2)
 {
     if(!Path)
         return 2;
