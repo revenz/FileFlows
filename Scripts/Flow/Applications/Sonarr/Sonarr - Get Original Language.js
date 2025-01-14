b370101aac6b14bf3559153ff9d05f2a315761ec
John Andrews <john@reven.co.nz>
2025-01-15 08:45:54 +1300

    Update Sonarr - Get Original Language.js
    updated revision and moved parameters to end to avoid breaking existing users

--- Scripts/Flow/Applications/Sonarr/Sonarr - Get Original Language.js
+++ Scripts/Flow/Applications/Sonarr/Sonarr - Get Original Language.js
@@ -4,16 +4,16 @@ import { Sonarr } from '../../../Shared/Sonarr';
 /**
  * @author reven 
  * @uid 51cf3c4f-f4a3-45e2-a083-6629397aab90
- * @revision 8
+ * @revision 9
  * @description Lookups a file in Sonarr and gets its original language ISO-693-1 code for it
- * @param {string} URL Sonarr root URL and port (e.g., http://sonarr:1234)
- * @param {string} ApiKey API Key for Sonarr
  * @param {string} Path The full file path to lookup in Sonarr
  * @param {bool} ISO2 If ISO-639-2 should be returned, otherwise ISO-639-1 will be used
+ * @param {string} URL Sonarr root URL and port (e.g., http://sonarr:1234)
+ * @param {string} ApiKey API Key for Sonarr
  * @output The language was found and stored in the variable OriginalLanguage
  * @output The language was not found
  */
-function Script(URI, ApiKey, Path, ISO2)
+function Script(Path, ISO2, URI, ApiKey)
 {
     URI = URI || Variables["Sonarr.Url"] || Variables["Sonarr.URI"];
     ApiKey = ApiKey || Variables["Sonarr.ApiKey"];
