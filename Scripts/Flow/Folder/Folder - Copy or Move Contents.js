fdb178151337252fdd4227cc9f8465b4af753cc4
Armand Welsh <armand@welshhome.org>
2025-02-02 21:25:52 -0800

    Update Folder - Copy or Move Contents.js
    Fixed typo in delete subdirectory variable.  Added logging.

--- Scripts/Flow/Folder/Folder - Copy or Move Contents.js
+++ Scripts/Flow/Folder/Folder - Copy or Move Contents.js
@@ -3,7 +3,7 @@
  * @uid e35d65b5-1486-42b9-8a0c-651804adea88
  * @description Copies or Movies a subset of files from a folder based on supplied filename
  * @author apwelsh
- * @revision 1
+ * @revision 2
  * @param {string} SourceDirectory The directory to copy (typically the folder being processed ex: {folder.FullName})
  * @param {string} DestinationDirectory The directory location to copy to (typically the location your servarr app is looking to for completed downloads)
  * @param {string} MediaFileName The file basename to copy all matching files of. The paths, and extension coponents will be ignored.  (typically {file.Name})
@@ -16,6 +16,8 @@ function Script(SourceDirectory, DestinationDirectory, MediaFileName, MoveFiles)
      let src = Flow.ReplaceVariables(SourceDirectory);
      let dest = System.IO.Path.Combine(Flow.ReplaceVariables(DestinationDirectory), System.IO.Path.GetFileName(src));
 
+      Logger.ILog(`Source Directors: ${src}`);
+      Logger.ILog(`Target Directory: ${dest}`);
 
      // Create filter of provided file basename, w/o extension, or * if none provided
      let filter = System.IO.Path.GetFileNameWithoutExtension(Flow.ReplaceVariables(MediaFileName || "*")) + ".*";
@@ -31,8 +33,10 @@ function Script(SourceDirectory, DestinationDirectory, MediaFileName, MoveFiles)
          let dstFile = System.IO.Path.Combine(dest, System.IO.Path.GetFileName(srcFile));
 
          if (MoveFiles) {
+             Logger.ILog(`Move File: ${srcFile} to: ${dstFile}`);
              System.IO.File.Move(srcFile, dstFile, true);
          } else {
+             Logger.ILog(`Copy File: ${srcFile} to: ${dstFile}`);
              System.IO.File.Copy(srcFile, dstFile, true);
          }
      }
@@ -53,11 +57,13 @@ function cleanDirectory(directory)
     {
         if (System.IO.Directory.GetFiles(subdir).Length == 0 && System.IO.Directory.GetDirectories(subdir).Length == 0)
         {
-            Directory.Delete(direcsubdirtory, false);
+            Logger.ILog(`Remove empty directory: ${subdir}`);
+            Directory.Delete(subdir, false);
         }
     }
     if (System.IO.Directory.GetFiles(directory).Length == 0 && System.IO.Directory.GetDirectories(directory).Length == 0)
     {
+        Logger.ILog(`Remove empty directory: ${directory}`);
         Directory.Delete(directory, false);
     }
 }
