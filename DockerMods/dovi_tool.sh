f575c4fae769c930542f328618d97acf19290f0e
Idan Bush <ibushari@outlook.com>
2025-03-10 11:36:59 +0000

    upgrade dovi_tool to 2.2.0 and change some of the prints

--- DockerMods/dovi_tool.sh
+++ DockerMods/dovi_tool.sh
@@ -1,33 +1,57 @@
 # ----------------------------------------------------------------------------------------------------
 # Name: dovi_tool
 # Description: dovi_tool is a CLI tool combining multiple utilities for working with Dolby Vision.
-# Revision: 6
+# Author: Lawrence Curtis / iBuSH
+# Revision: 7
 # Icon: data:image/svg+xml;base64,PCFET0NUWVBFIHN2ZyBQVUJMSUMgIi0vL1czQy8vRFREIFNWRyAxLjEvL0VOIiAiaHR0cDovL3d3dy53My5vcmcvR3JhcGhpY3MvU1ZHLzEuMS9EVEQvc3ZnMTEuZHRkIj4KDTwhLS0gVXBsb2FkZWQgdG86IFNWRyBSZXBvLCB3d3cuc3ZncmVwby5jb20sIFRyYW5zZm9ybWVkIGJ5OiBTVkcgUmVwbyBNaXhlciBUb29scyAtLT4KPHN2ZyBmaWxsPSIjZmZmZmZmIiB3aWR0aD0iODAwcHgiIGhlaWdodD0iODAwcHgiIHZpZXdCb3g9IjAgMCAyNCAyNCIgcm9sZT0iaW1nIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHN0cm9rZT0iI2ZmZmZmZiI+Cg08ZyBpZD0iU1ZHUmVwb19iZ0NhcnJpZXIiIHN0cm9rZS13aWR0aD0iMCIvPgoNPGcgaWQ9IlNWR1JlcG9fdHJhY2VyQ2FycmllciIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIi8+Cg08ZyBpZD0iU1ZHUmVwb19pY29uQ2FycmllciI+Cg08cGF0aCBkPSJNMjQgMjAuMzUyVjMuNjQ4SDB2MTYuNzA0aDI0ek0xOC40MzMgNS44MDZoMi43MzZ2MTIuMzg3aC0yLjczNmMtMi44MzkgMC01LjIxNC0yLjc2Ny01LjIxNC02LjE5NHMyLjM3NS02LjE5MyA1LjIxNC02LjE5M3ptLTE1LjYwMiAwaDIuNzM2YzIuODM5IDAgNS4yMTQgMi43NjcgNS4yMTQgNi4xOTRzLTIuMzc0IDYuMTk0LTUuMjE0IDYuMTk0SDIuODMxVjUuODA2eiIvPgoNPC9nPgoNPC9zdmc+
 # ----------------------------------------------------------------------------------------------------
 
 #!/bin/bash
 
+# Change Version of dovi_tool
+VERSION=2.2.0
+
+# Function to handle errors
+function handle_error {
+    echo "An error occurred. Exiting..."
+    exit 1
+}
+
 # Check if the --uninstall option is provided
 if [ "$1" == "--uninstall" ]; then
-    echo "Uninstalling..."
-    rm -f /bin/dovi_tool
-    exit 0
+    echo "Uninstalling dovi_tool..."
+    if rm -f /bin/dovi_tool; then
+        echo "dovi_tool successfully uninstalled."
+        exit 0
+    else
+        handle_error
+    fi
 fi
 
-# Check if rar is installed
+# Check if dovi_tool is installed
 if command -v dovi_tool &>/dev/null; then
     echo "already installed."
     exit 0
 fi
 
-wget -O /tmp/dovi_tool.tar.gz https://github.com/quietvoid/dovi_tool/releases/download/2.1.3/dovi_tool-2.1.3-x86_64-unknown-linux-musl.tar.gz
+echo "dovi_tool is not installed. Installing..."
+
+# Update package lists and install dependencies
+if ! apt update || ! apt install -y libfontconfig-dev libdovi; then
+    handle_error
+fi
+
+# Install dovi_tool
+wget -O /tmp/dovi_tool.tar.gz https://github.com/quietvoid/dovi_tool/releases/download/${VERSION}/dovi_tool-${VERSION}-x86_64-unknown-linux-musl.tar.gz
 tar xvf /tmp/dovi_tool.tar.gz
 mv dovi_tool /bin
 rm /tmp/dovi_tool.tar.gz
 
+echo "Installation complete."
+
 # Verify installation
 if command -v dovi_tool &>/dev/null; then
-    echo "successfully installed."
+    echo "Successfully installed."
     exit 0
 fi
 
