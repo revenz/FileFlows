db6b52ad57ef4559a45b0fe8c6b0f674ff04e018
Thornleigh0 <wrexplorer@gmail.com>
2025-03-30 19:16:42 -0400

    Update Video - Has 5.1 Audio.js
    Added a tolerance window so that the check will still resolve true when channel counts report as long decimals due to JavaScript floating point issues. 
    E.G. "Audio channel found: 5.09999990463256" =/= 5.1 
    //Returns Output 2

--- Scripts/Flow/Video/Video - Has 5.1 Audio.js
+++ Scripts/Flow/Video/Video - Has 5.1 Audio.js
@@ -2,24 +2,26 @@
  * @author reven
  * @uid c87f9e27-6587-490b-a51e-f65e7542b891
  * @description Determines if a video has a 5.1 channel track
- * @revision 6
+ * @revision 7
  * @output Does have 5.1
  * @output Does not have 5.1
  */
 function Script() 
 {
   let audioStreams = Variables.vi?.VideoInfo?.AudioStreams;
+  const TARGET_CHANNELS = 5.1;
+  const TOLERANCE = 0.03;
+  
   if (!audioStreams) {
     Logger.ILog("no audio streams");
     return 2;
   }
-
   for (let i = 0; i < audioStreams.length; i++) {
     let audio = audioStreams[i];
     if(audio.Deleted)
       continue;
     Logger.ILog("Audio channel found: " + audio.Channels);
-    if (audio.Channels === 5.1)
+    if (Math.abs(audio.Channels - TARGET_CHANNELS) <= TOLERANCE)
       return 1;
   }
 
