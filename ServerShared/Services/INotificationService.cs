08673e10ff408202a7bf0d470f7d97e786ee1670
John Andrews <john@reven.co.nz>
2025-04-16 09:15:13 +1200

    FF-2199: Library Scan Notification

--- ServerShared/Services/INotificationService.cs
+++ ServerShared/Services/INotificationService.cs
@@ -14,4 +14,14 @@ public interface INotificationService
     /// <param name="title">The title of the notification.</param>
     /// <param name="message">The message content of the notification.</param>
     Task Record(NotificationSeverity severity, string title, string? message = null);
+    
+    /// <summary>
+    /// Records a new notification with the specified severity, title, and message.
+    /// </summary>
+    /// <param name="identifier">unique identifier to limit how often these messages are shown</param>
+    /// <param name="frequency">the frequency of how often the messages can be shown</param>
+    /// <param name="severity">The severity level of the notification.</param>
+    /// <param name="title">The title of the notification.</param>
+    /// <param name="message">The message content of the notification.</param>
+    Task Record(string identifier, TimeSpan frequency, NotificationSeverity severity, string title, string? message = null);
 }
